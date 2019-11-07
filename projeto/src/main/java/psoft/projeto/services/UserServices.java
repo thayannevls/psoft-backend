ca4dbd252b0b124d7afb0f3a21b42efbfd63fc90
Vitor Diniz <vitor.diniz96@gmail.com>
2019-11-07 15:06:33 -0300

    InsertUser retorna usuário

--- projeto/src/main/java/psoft/projeto/services/UserServices.java
+++ projeto/src/main/java/psoft/projeto/services/UserServices.java
@@ -21,21 +21,26 @@ public class UserServices {
     return users;
   }
 
-  public void insertUser(String firstName, String lastName, String email, long cartao, String senha) {
+  public User insertUser(String firstName, String lastName, String email, long cartao, String senha) {
+    User user = null;
     if (!this.users.containsKey(email)) {
-      User user = new User(firstName, lastName, email, cartao, senha);
+      user = new User(firstName, lastName, email, cartao, senha);
       users.put(email, user);
       String msg = "Seja bem vindo(a) ao AJuDE!!";
       this.sendEmail(user.getEmail(), msg);
     }
+    return user;
   }
 
-  public void insertUser(User user) {
+  public User insertUser(User user) {
     if (!this.users.containsKey(user.getEmail())) {
       users.put(user.getEmail(), user);
       String msg = "Seja bem vindo(a) ao AJuDE!!";
       this.sendEmail(user.getEmail(), msg);
+    } else{
+      user = null;
     }
+    return user;
   }
 
 
