272556670b9a5eb82378990538e04e7692e066c3
Thayanne <thayannevls@gmail.com>
2019-11-13 19:11:30 -0300

    habilita cors

--- AJuDE/src/main/java/psoft/ufcg/api/AJuDE/AJuDeApplication.java
+++ AJuDE/src/main/java/psoft/ufcg/api/AJuDE/AJuDeApplication.java
@@ -2,9 +2,27 @@ package psoft.ufcg.api.AJuDE;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class AJuDeApplication {
+	
+	@SuppressWarnings("rawtypes")
+	@Bean
+	public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration().applyPermitDefaultValues();
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(0);
+
+        return bean;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AJuDeApplication.class, args);
