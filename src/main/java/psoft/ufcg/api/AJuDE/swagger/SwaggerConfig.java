2433f67c4df54074bfe4b2aeaa085e86d0c2e412
Thayanne <thayannevls@gmail.com>
2019-11-23 00:03:56 -0300

    aumenta versao do swagger

--- src/main/java/psoft/ufcg/api/AJuDE/swagger/SwaggerConfig.java
+++ src/main/java/psoft/ufcg/api/AJuDE/swagger/SwaggerConfig.java
@@ -28,8 +28,8 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
 
   private ApiInfo metaData() {
     return new ApiInfoBuilder()
-        .title("Spring Boot REST API")
-        .description("\"Spring Boot REST API for greeting people\"")
+        .title("AJuDE API")
+        .description("Plataforma colaborativa para crowdfunding")
         .version("1.0.0")
         .license("Apache License Version 2.0")
         .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
