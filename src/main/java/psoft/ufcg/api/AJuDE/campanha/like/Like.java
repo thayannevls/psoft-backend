e1d456e7432302ed3f4b16cf9800352913a3fb86
Vitor Diniz <vitor.diniz96@gmail.com>
2019-11-28 06:44:48 -0300

    Like  devidamente documentada

--- src/main/java/psoft/ufcg/api/AJuDE/campanha/like/Like.java
+++ src/main/java/psoft/ufcg/api/AJuDE/campanha/like/Like.java
@@ -9,23 +9,30 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
 import psoft.ufcg.api.AJuDE.campanha.Campanha;
 import psoft.ufcg.api.AJuDE.usuario.Usuario;
 
+@ApiModel(value = "Like", description = "Modelo de um like. Esse modelo representa a entidade like no banco de dados e possui as funções básicas de getters e setters para seus atributos.")
 @Entity
 @Table(name = "tb_like")
 public class Like {
+
+	@ApiModelProperty(value = "Identificador único do like.", example = "2313542343", position = 0)
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
+	@ApiModelProperty(value = "Campanha a qual recebe o like", example = "Campanha do Milhão", position = 1)
 	@ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "campanha_id", nullable = false)
     @OnDelete(action = OnDeleteAction.CASCADE)
 	private Campanha campanha;
-	
+
+	@ApiModelProperty(value = "Usuário que deu like", example = "Aderbal Pinto Filgueiras", position = 2)
 	@ManyToOne(fetch = FetchType.LAZY, optional = false)
 	@JoinColumn(name = "usuario_email", nullable = false)
 	private Usuario usuario;
