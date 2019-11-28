cd6172ab30932a98f7245043deedb04ebb50979c
Vitor Diniz <vitor.diniz96@gmail.com>
2019-11-28 06:50:49 -0300

    Doacao devidamente documentada

--- src/main/java/psoft/ufcg/api/AJuDE/doacao/Doacao.java
+++ src/main/java/psoft/ufcg/api/AJuDE/doacao/Doacao.java
@@ -9,6 +9,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
@@ -17,23 +19,29 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import psoft.ufcg.api.AJuDE.campanha.Campanha;
 import psoft.ufcg.api.AJuDE.usuario.Usuario;
 
+@ApiModel(value = "Doação", description = "Modelo de uma doação. Esse modelo representa a entidade doação no banco de dados e possui as funções básicas de getters e setters para seus atributos.")
 @Entity
 @Table(name = "tb_doacao")
 public class Doacao {
+
+	@ApiModelProperty(value = "Identificador único da doação", example = "653423", position = 0)
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
+	@ApiModelProperty(value = "Usuário que realizou a doação", example = "José Vicente de Faria Lima", position = 1)
 	@ManyToOne(fetch = FetchType.EAGER, optional = false)
     @JoinColumn(name = "usuario_email", nullable = false)
     @OnDelete(action = OnDeleteAction.CASCADE)
 	@JsonIgnore
 	private Usuario doador;
 
+	@ApiModelProperty(value = "Campanha que recebeu a doação", example = "Campanha do Milhão", position = 2)
 	@ManyToOne(fetch = FetchType.EAGER, optional = false)
     @JoinColumn(name = "campanha_id", nullable = false)
     @OnDelete(action = OnDeleteAction.CASCADE)
 	@JsonIgnore
 	private Campanha campanha;
+	@ApiModelProperty(value = "Valor doado em reais", example = "0.01", position = 3)
 	private double valor;
 
 	public Doacao(Usuario doador, Campanha campanha, double valor) {
